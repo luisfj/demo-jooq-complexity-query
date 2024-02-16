7e72f3ce1cce1188d605dd5064cbd8f86f32e27d
luis <luisfj_pr@hotmail.com>
2024-02-16 14:01:53 +0000

    :tada: init: initial commit

---
+++ demo-jooq-complexity-query/src/main/java/dev/luisjohann/demojooqcomplexityquery/DemoJooqComplexityQueryApplication.java
@@ -0,0 +1,13 @@
+package dev.luisjohann.demojooqcomplexityquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJooqComplexityQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJooqComplexityQueryApplication.class, args);
+	}
+
+}
---
+++ demo-jooq-complexity-query/src/test/java/dev/luisjohann/demojooqcomplexityquery/DemoJooqComplexityQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luisjohann.demojooqcomplexityquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJooqComplexityQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
